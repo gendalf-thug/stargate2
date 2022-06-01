4d523c14efeb73d324bac66bdbbefc887b4f4662
taZik616 <taZik-616@yandex.ru>
2022-06-01 18:54:50 +0500

    remove 1 line

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query/react'
 import { movieApi } from './movieApi'
 
 export const store = configureStore({
